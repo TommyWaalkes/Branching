16518a1e0a005d8c988d8785238980c86491ac9e
Tommy Waalkes <Tommy@grandcircus.co>
2020-02-19 16:03:51 -0500

    Added the rest of the project

---
+++ TommySimCore/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TommySimCore
+{
+    class City
+    {
+        public int Water { get; set; }
+        public int Wood { get; set; }
+
+        public List<Person> Villagers = new List<Person>();
+
+        public City()
+        {
+            Water = 10;
+        }
+    }
+}
---
+++ TommySimCore/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TommySimCore
+{
+    class Person
+    {
+        public string Name { get; }
+        
+    }
+}
